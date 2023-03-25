03c4643c371259005d3b87835d89852883753d38
Ahmad Yasi Faizi <yasifaizi10@gmail.com>
2023-03-25 16:54:19 +0700

    Fixed Table

--- src/components/TaskPage.jsx
+++ src/components/TaskPage.jsx
@@ -5,6 +5,9 @@ import "./HomePage.css"
 const TaskPage = () => {
   const [projects, setProjects] = useState([]);
   const [projectName, setProjectName] = useState("");
+  const [projectdes, setProjectdes] = useState("");
+  const [projectPriority, setProjectPriority] = useState("");
+  const [projectstatus, setProjectstatus] = useState("");
   const [isAddingProject, setIsAddingProject] = useState(false);
   const [editingProject, setEditingProject] = useState(null);
 
@@ -17,19 +20,37 @@ const TaskPage = () => {
   };
 
   const handleSaveProject = () => {
-    const newProject = { name: projectName };
+    const newProject = { name: projectName, des: projectdes, priority: projectPriority, status: projectstatus};
     setProjects([...projects, newProject]);
     setIsAddingProject(false);
     setProjectName("");
+    setProjectdes("");
+    setProjectPriority("");
+    setProjectstatus("");
   };
 
   const handleProjectNameChange = (event) => {
     setProjectName(event.target.value);
   };
+  
+  const handleProjectdesChange = (event) => {
+    setProjectdes(event.target.value);
+  };
+  
+  const handleProjectPriorityChange = (event) => {
+    setProjectPriority(event.target.value);
+  };
+  const handleProjectStatusChange = (event) => {
+    setProjectstatus(event.target.value);
+  };
+
 
   const handleViewProject = (project) => {
     setEditingProject(project);
     setProjectName(project.name);
+    setProjectdes(project.des);
+    setProjectPriority(project.priority);
+    setProjectstatus(project.status);
   };
 
   const handleDeleteProject = (project) => {
@@ -42,26 +63,25 @@ const TaskPage = () => {
     setProjects((prevProjects) =>
       prevProjects.map((prevProject) => {
         if (prevProject === editingProject) {
-          return { ...prevProject, name: projectName };
+          return { ...prevProject, name: projectName, des: projectdes, priority: projectPriority, status: projectstatus };
         }
         return prevProject;
       })
     );
     setEditingProject(null);
     setProjectName("");
+    setProjectdes("");
+    setProjectPriority("");
+    setProjectstatus("");
   };
 
   return (
-    
     <div className="home-page">
-      
-      
       {/* existing code */}
-      
-      
-        <Link to="/ProfilePage"><button className="profile-button">Profile</button> </Link>
-        <h3>My Specific Project</h3>
-      
+      <Link to="/ProfilePage">
+        <button className="profile-button">Profile</button>
+      </Link>
+      <h3>My Specific Project</h3>
       <button onClick={handleAddProject}>Add Task</button>
       {isAddingProject || editingProject ? (
         <div>
@@ -69,7 +89,36 @@ const TaskPage = () => {
             type="text"
             value={projectName}
             onChange={handleProjectNameChange}
+            placeholder="Enter task name"
           />
+          <input
+            type="text"
+            value={projectdes}
+            onChange={handleProjectdesChange}
+            placeholder="Enter task description"
+          />
+          {isAddingProject ? (
+            <select
+              value={projectPriority}
+              onChange={handleProjectPriorityChange}
+            >
+              <option value="">Select priority level</option>
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          ) : null}
+          {isAddingProject ? (
+            <select
+              value={projectstatus}
+              onChange={handleProjectStatusChange}
+            >
+              <option value="">Select Status</option>
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+              
+            </select>
+          ) : null}
           {editingProject ? (
             <button onClick={handleUpdateProject}>Update</button>
           ) : (
@@ -77,40 +126,73 @@ const TaskPage = () => {
           )}
           <button onClick={handleCancelAddProject}>Cancel</button>
         </div>
-      ) : null}
+      ): null}
       {projects.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Task</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {projects.map((project, index) => (
-              <tr key={index}>
-                <td>{project.name}</td>
-                <td>
-                  <button onClick={() => handleViewProject(project)}>
-                    Edit
-                  </button>
-                </td>
-                <td>
-                  <button onClick={() => handleDeleteProject(project)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <table>
+      <thead>
+      <tr>
+      <th>Task</th>
+      <th>Description</th>
+      <th>Priority Level</th>
+      <th>Completion Status</th>
+      <th>Edit</th>
+      <th>Delete</th>
+      </tr>
+      </thead>
+      <tbody>
+      {projects.map((project, index) => (
+      <tr key={index}>
+      <td>{project.name}</td>
+      <td>{project.des}</td>
+      <td>
+      {editingProject && editingProject === project ? (
+      <select
+                         value={projectPriority}
+                         onChange={handleProjectPriorityChange}
+                       >
+      <option value="">Select priority level</option>
+      <option value="low">Low</option>
+      <option value="medium">Medium</option>
+      <option value="high">High</option>
+      </select>
       ) : (
-        <p>No Tasks found.</p>
+      project.priority
       )}
+      </td>
+      <td>
+      {editingProject && editingProject === project ? (
+      <select
+                         value={projectstatus}
+                         onChange={handleProjectStatusChange}
+                       >
+      <option value="">Select Status</option>
+      <option value="Yes">Yes</option>
+      <option value="No">No</option>
       
-    </div>
-  );
-};
-
-export default TaskPage;
+      </select>
+      ) : (
+      project.status
+      )}
+      </td>
+      <td>
+      <button onClick={() => handleViewProject(project)}>
+      Edit
+      </button>
+      </td>
+      <td>
+      <button onClick={() => handleDeleteProject(project)}>
+      Delete
+      </button>
+      </td>
+      </tr>
+      ))}
+      </tbody>
+      </table>
+      ) : (
+      <p>No Tasks found.</p>
+      )}
+      </div>
+      );
+      };
+      
+      export default TaskPage;
